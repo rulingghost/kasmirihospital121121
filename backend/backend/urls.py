3e29eb1b8955f282005f415b6bb08183507054d4
Yusuf <ahmetyusuf.kuru@gmail.com>
2025-12-09 19:04:23 +0300

    Add root view and wsgi app alias

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Backend Running!")
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('hospital.urls')),
     path("api-auth/",include('rest_framework.urls')),
